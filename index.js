9c77bbf3e4c41d371bae949d670bb0b45fe34bb2
neotech-starassov <neotech.starassov@gmail.com>
2017-08-09 00:11:47 +0300

    Update index.js
    Add evaluation params & context functionality.

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+var loaderUtils = require('loader-utils')
+var defaultOptions = {
+  params: null,
+  context: null
+};
+
 module.exports = function(source) {
   this.cacheable();
   var module2 = {};
   source.replace('module.exports','module2.exports');
-  var evaled = eval(source);
-  return evaled.exports;
+
+  var options = Object.assign(
+    {},
+    defaultOptions,
+    loaderUtils.getOptions(this),
+  );
+
+  var params = options.params;
+  var paramsNames = ['module'];
+  var module = {exports: {}};
+  var paramsValues = [module];
+  
+  if( (typeof params === "object" && params !== null) {
+    paramsNames = paramsNames.concat(Object.keys(options));
+    paramsValues = paramsValues.concat(paramsNames.map(function(key) {
+      return params[key];
+    }));
+  }
+
+  (new (Function.prototype.bind.apply(Function, paramsNames))).apply(params.context, paramsValues);
+
+  return module.exports;
 };
